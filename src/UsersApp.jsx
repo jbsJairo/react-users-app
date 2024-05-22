b256ce2570351a67167976b8cab8f9d06b1255ae
jbsJairo <jbsegura1990@gmail.com>
2024-05-22 16:37:49 -0500

    128. Service Auth

--- src/UsersApp.jsx
+++ src/UsersApp.jsx
@@ -1,36 +1,26 @@
-import { useReducer } from "react"
 import { LoginPage } from "./auth/pages/LoginPage"
-import { loginReducer } from "./auth/reducers/loginReducer"
-import Swal from "sweetalert2"
 import { UsersPage } from "./pages/UsersPage"
-
-
-const initialLogin = {
-    isAuth: false,
-    user: undefined,
-}
+import { Navbar } from "./components/layout/Navbar"
+import { useLogin } from "./auth/hooks/useLogin"
 
 export const UsersApp = () => {
 
-    const [ login, dispach ] = useReducer(loginReducer, initialLogin);
-
-    const handlerLogin = ( { username, password } ) => {
-        if(username === 'admin' && password === '12345'){
-            const user = { username: 'admin' };
-            dispach({
-                type: 'login',
-                payload: user,
-            });
-        }else{
-            Swal.fire('Error Login', 'Username o password invalidos', 'error');
-        }
-    }
+    const {
+        login,
+        handlerLogin,
+        handlerLogout,
+    } = useLogin();
 
     return(
         <>
         { 
             login.isAuth
-                ? <UsersPage />
+                ? (
+                    <>
+                        <Navbar login={ login } handlerLogout={ handlerLogout }/>
+                        <UsersPage />
+                    </>
+                )                
                 : <LoginPage handlerLogin={ handlerLogin }/>
         }
         </>
